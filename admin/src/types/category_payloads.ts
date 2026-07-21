2fab2c57bf7f94fc4312f3140afde24c24ad8c7f
Youssef-Ahmed-Zahran <youssefzahran480@gmail.com>
2026-07-22 01:58:53 +0300

    feat(categories): add isActive to CreateCategoryPayload and CreateSubCategoryPayload types

--- admin/src/types/category_payloads.ts
+++ admin/src/types/category_payloads.ts
@@ -1,12 +1,14 @@
 export interface CreateCategoryPayload {
   name: string;
   image?: string;
+  isActive?: boolean;
 }
 
 export interface CreateSubCategoryPayload {
   name: string;
   parentId: string;
   image?: string;
+  isActive?: boolean;
 }
 
 export interface LinkStorePayload {
